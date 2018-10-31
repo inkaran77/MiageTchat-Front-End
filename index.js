6910dcb9d10117cb28b81abf6b5ea8bd372fba09
Inkaran Thuraiyappah <inkaran.thuraiyappah@edf.fr>
2018-10-31 10:03:07 +0100

    Test Vue.js

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-var app = new Vue({
-    el: '#app'
+var vm = new Vue({
+    el: '#app',
     data: {
-        message : 'Test'
+      firstName: 'Inkaran',
+      lastName: 'Thuraiyappah'
+    },
+    computed: {
+        fullName: {
+            // getter
+            get: function () {
+              return this.firstName + ' ' + this.lastName
+            },
+            // setter
+            set: function (newValue) {
+              var names = newValue.split(' ')
+              this.firstName = names[0]
+              this.lastName = names[names.length - 1]
+            }
+          }
     }
-})
+  })
+  
+  console.log(vm.fullName)
+  
+  
+  var fonction=setInterval(function(){
+            vm.fullName="test "+"test";
+  },5000);
+  
+  var fonction=setInterval(function(){
+            vm.fullName="test1 "+"test1";
+  },4000);
+  
+     
+  //var interval1Id = setInterval(function(){
+    //console.log(vm.fullName="test "+t );
+    //},5000);
